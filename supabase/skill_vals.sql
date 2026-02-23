48a7f1116a14de67f800ea92e91f66c73d7af89f
Yashaswe Amatya <amatyayasu@gmail.com>
2026-02-22 21:23:37 -0500

    Insert values for skill definitions into database

---
+++ supabase/skill_vals.sql
@@ -0,0 +1,11 @@
+INSERT INTO skill_definitions (org_id, name)
+VALUES
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Submerging'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Rhythmic Breathing'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Front float'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Front Glide'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Back Float'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Back Glide'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Roll from back float to front'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Roll from front float to back'),
+('37066922-8a50-4cab-baaf-7fd05fa06999', 'Kicking');
